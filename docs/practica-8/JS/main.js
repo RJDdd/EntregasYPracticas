19676ed13025fce4d4bdd8b0b2000866ee07816e
RJDdd <cdmx2605@amerike.edu.mx>
2025-03-20 09:44:14 -0600

    8-1

--- docs/practica-8/JS/main.js
+++ docs/practica-8/JS/main.js
@@ -1,57 +1,85 @@
 const d = document;
-const $listaCarrito = d.querySelector("#lista-carrito");
-const $totalCarrito = d.querySelector("#total-carrito");
-const $btnCompra = d.querySelector("#btn-compra");
-const $mensajeCompra = d.querySelector("#mensaje-compra");
-const $carrito = d.querySelector("#carrito");
+const $form = d.querySelector("#register-form");
+const $nameInput = d.querySelector("#name");
+const $nameError = d.querySelector("#name-error");
+const $emailInput = d.querySelector("#email");
+const $emailError = d.querySelector("#email-error");
+const $passwordInput = d.querySelector("#password");
+const $passwordError = d.querySelector("#password-error");
+const $confirmPasswordInput = d.querySelector("#confirm-password");
+const $confirmPasswordError = d.querySelector("#confirm-password-error");
+const $successMessage = d.querySelector("#success-message");
+const $errorsMessages = d.querySelectorAll(".error");
 
-$btnCompra.addEventListener("click", function (e) {
-  //alert("Presionando Botón");
+// Función de Validación del Formulario
+function validateForm(e) {
+  // Cancela el comportamiento por defecto del evento
+  e.preventDefault();
   console.log(e);
 
-  if ($listaCarrito.children.length > 0) {
-    $mensajeCompra.classList.remove("hidden");
-    setTimeout(function () {
-      $mensajeCompra.classList.add("hidden");
-      $listaCarrito.innerHTML = "";
-      $totalCarrito.textContent = "0";
-    }, 3000);
-  } else {
-    alert("El carrito esta vacío");
-  }
-});
-
-/* 
-No apliques un evento a varios elementos mediante un loop, por que es una mala práctica, si quieres asinar un evento a varios elementos usa la DELEGACIÓN DE EVENTOS
-
-const $productos = d.querySelectorAll(".producto");
-console.log($productos);
-
-$productos.forEach(function (el) {
-  el.addEventListener("click", function (e) {
-    alert("Presionando Producto");
+  //Limpiamos los mensajes de error de los inputs
+  $errorsMessages.forEach((el) => {
+    el.textContent = "";
   });
-}); */
 
-d.addEventListener("click", function (e) {
-  if (e.target.matches(".producto")) {
-    console.log(e);
+  let isValid = true;
 
-    const $producto = e.target;
-    let nombre = $producto.getAttribute("data-nombre");
-    let precio = parseFloat($producto.getAttribute("data-precio"));
-
-    // Crear un elemento de lista para el carrito
-    const $itemCarrito = d.createElement("li");
-    $itemCarrito.textContent = `${nombre} - $${precio}`;
-
-    // Insertar el elemento al carrito
-    $listaCarrito.appendChild($itemCarrito);
-
-    // Actualizar el total del carrito
-    let totalActual = parseFloat($totalCarrito.textContent);
-    $totalCarrito.textContent = totalActual + precio;
+  //Validar campo Nombre
+  if ($nameInput.value.trim() === "") {
+    $nameError.textContent = "El nombre es obligatorio";
+    $nameInput.focus();
+    isValid = false;
   }
 
-  return false;
-});
+  //Validar campo Correo
+  let emailPattern = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+  if ($emailInput.value.trim() === "") {
+    $emailError.textContent = "El correo es obligatorio";
+    $emailInput.focus();
+    isValid = false;
+  } else if (!emailPattern.test($emailInput.value.trim())) {
+    $emailError.textContent = "El formato del correo es inválido";
+    $emailInput.focus();
+    isValid = false;
+  }
+
+  //Validar campo Contraseña
+  if ($passwordInput.value.trim() === "") {
+    $passwordError.textContent = "La Contraseña es obligatoria";
+    $passwordInput.focus();
+    isValid = false;
+  } else if ($passwordInput.value.trim().length < 8) {
+    $passwordError.textContent =
+      "La Contraseña al menos debe tener 8 caracteres";
+    $passwordInput.focus();
+    isValid = false;
+  }
+
+  //Validar campo Confirmar Contraseña
+  if ($confirmPasswordInput.value.trim() === "") {
+    $confirmPasswordError.textContent =
+      "La confirmación de la Contraseña es obligatoria";
+    $confirmPasswordInput.focus();
+    isValid = false;
+  } else if (
+    $confirmPasswordInput.value.trim() !== $passwordInput.value.trim()
+  ) {
+    $confirmPasswordError.textContent =
+      "La confirmación de la Contraseña no coincide con el valor de la contraseña";
+    $confirmPasswordInput.focus();
+    isValid = false;
+  }
+
+  //Enviando el formulario
+  if (isValid) {
+    alert("Enviando el formulario");
+    $successMessage.textContent = "Formulario enviado exitosamente";
+    $form.reset();
+    setTimeout(() => {
+      $successMessage.textContent = "";
+      $form.submit();
+    }, 3000);
+  }
+}
+
+$form.addEventListener("submit", validateForm);
